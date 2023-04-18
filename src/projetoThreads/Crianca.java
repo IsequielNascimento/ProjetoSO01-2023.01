12e23428b2f1b6157a180b37dc09dfa1fabd8738
IsequielNascimento <isequielnascimento@gmail.com>
2023-04-17 21:40:01 -0300

    Início do padrão do projeto na classe criança

--- src/projetoThreads/Crianca.java
+++ src/projetoThreads/Crianca.java
@@ -1,5 +1,39 @@
 package projetoThreads;
+import java.util.concurrent.Semaphore;
 
-public class Crianca {
-    
+public class Crianca extends Thread {
+
+    private String nome;
+    private boolean temBola;
+    private int tempoBrincadeira; //tb
+    private int tempoQuieta; //tq
+
+    public Crianca(String nome, boolean tembola, int tempoBrincadeira, int tempoQuieta){
+        this.nome = nome;
+        this.temBola = tembola;
+        this.tempoBrincadeira = tempoBrincadeira;
+        this.tempoQuieta = tempoQuieta;
+    }
+
+    public void run(){
+
+    }
+
+    public void brincar() throws InterruptedException{
+        System.out.println("A" + nome + " está brincando com a bola");
+        Thread.sleep(tempoBrincadeira);
+    }
+
+    public void descansar() throws InterruptedException{
+        System.out.println("A" + nome + " está quieta");
+        Thread.sleep(tempoQuieta);
+    }
+
+
+
+    public static void main(String[] args){
+        Crianca teste = new Crianca();
+        teste.start();
+    }
+
 }
