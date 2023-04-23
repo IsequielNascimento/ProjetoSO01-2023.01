9927b8a259d25cd9554a416779255e7cc18a4a9f
Honorio Filho <honorio.vitor.nascimento07@aluno.ifce.edu.br>
2023-04-23 16:24:45 -0300

    Ajuste da função Run()

--- src/main/java/threads/Crianca.java
+++ src/main/java/threads/Crianca.java
@@ -2,84 +2,103 @@
 
 package threads;
 import gui.resources.CriancaConcrete;
-
-
+import gui.resources.assets.MainGUI;
 import java.util.List;
 import java.util.concurrent.Semaphore;
-
 // javax.swing is a package that contains classes for creating graphical user interfaces
 import javax.swing.*;
 
 public class Crianca extends Thread {
 
-
-
-
+    public static int K;
+    public static Semaphore bolas = new Semaphore(0);
+    public static Semaphore cesto = new Semaphore(2);
+    public static List<Crianca> criancas;
     private String nome;
     private boolean temBola;
     private int tempoBrincadeira; //tb
     private int tempoQuieta; //tq
     // public static int totalCrianca = 0;
 
-    public Crianca(String nome, boolean temBola, int tempoBrincadeira, int tempoQuieta){
+
+    //Construtor
+    public Crianca(String nome, boolean temBola, int tempoBrincadeira, int tempoQuieta, Semaphore cesto, Semaphore bolas){
         this.nome = nome;
         this.temBola = temBola;
         this.tempoBrincadeira = tempoBrincadeira;
         this.tempoQuieta = tempoQuieta;
-       // totalCrianca++;
+        this.cesto = cesto;
+        this.bolas = bolas;
     }
 
-    public static List<Crianca> criancas;
-    public static Semaphore mutex = new Semaphore(1);
-    public static Semaphore cesto = new Semaphore(0);
+    public static void set_tamCesto(){
+        K = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a capacidade do cesto",
+                "Entrada inicial", JOptionPane.INFORMATION_MESSAGE));
+    }
 
-
+    public static int get_tamCesto(){
+        return K;
+    }
 
     //Método que inicia a brincadeira das crianças. OBS: O método é temporário para testes
     public void run(){
-
         try{
             while(true){
                 if(temBola){
                     brincar();
-                    //mutex iria para 0 porque dessa forma nenhuma criança pega no cesto
-                    temBola = false;
-                    //mutex iria para 1 porque assim outra criança pega a bola no cesto
+                    colocarBola();
+                    descansar();
                 }else{
+                    esperarBola();
                     pegarBola();
+                    brincar();
+                    colocarBola();
+                    descansar();
                 }
-                descansar();
             }
         }catch(InterruptedException e){
             System.out.println("A criança " + nome + " foi interrompida");
         }
-
-
     }
 
-    public void brincar() throws InterruptedException{
-        System.out.println("A " + nome + " está brincando com a bola");
-
+    private void brincar() throws InterruptedException{
+        System.out.println("Criança " + nome + " está brincando com a bola");
         Thread.sleep(tempoBrincadeira);
     }
 
-    public void descansar() throws InterruptedException{
+    private void descansar() throws InterruptedException{
         System.out.println("A " + nome + " está quieta");
         Thread.sleep(tempoQuieta);
     }
 
-    public void pegarBola() throws InterruptedException{
-        System.out.println("A "+nome+ " vai pegar uma bola");
-        Semaforo cesto = new Semaforo();
-        cesto.cestoLim.acquire();
-        System.out.println("A criança pegou a bola");
-        //mutex iria para 0 porque dessa forma nenhuma criança pega no cesto
-        temBola = true;
-        //mutex iria para 1 porque assim outra criança pega a bola no cesto
+    private void esperarBola() throws InterruptedException{
+        // Esperar para pegar a bola do cesto
+        System.out.println("Criança " + nome + " está esperando para pegar a bola do cesto");
+        bolas.acquire();
     }
+
+    private void pegarBola() throws InterruptedException{
+        cesto.acquire();
+        System.out.println("Criança " + nome + " pegou a bola do cesto");
+        temBola = true;
+        cesto.release();
+    }
+
+
+    private void colocarBola() throws InterruptedException{
+        // Colocar a bola no cesto
+        cesto.acquire();
+        System.out.println("Criança " + nome + " está colocando a bola no cesto");
+        temBola = false;
+        bolas.release();
+        cesto.release();
+    }
+
+    private void setVisible(boolean b) {}
 
     public static void main(String[] args){
         //javax.swing.SwingUtilities is a class that contains static methods for creating and managing top-level windows
+        //set_tamCesto();
         SwingUtilities.invokeLater(()->{
 
                 //CriancaForm is a class that represents a window
@@ -89,9 +108,6 @@ public class Crianca extends Thread {
 
         });
         System.out.println("Olá mundo");
-    }
-
-    private void setVisible(boolean b) {
     }
 
 }
