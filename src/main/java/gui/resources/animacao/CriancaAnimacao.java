9ed88caaefd28f47c325ad2ec78ae49ea62d4450
IsequielNascimento <isequielnascimento@gmail.com>
2023-04-23 19:07:57 -0300

    afeat: adição de cesto e atualização da área percorrida pela criança

--- src/main/java/gui/resources/animacao/CriancaAnimacao.java
+++ src/main/java/gui/resources/animacao/CriancaAnimacao.java
@@ -8,19 +8,25 @@ import java.awt.event.*;
 public class CriancaAnimacao extends JPanel implements ActionListener {
 
     private Timer timer;
-    private int x = 0;
-    private int y = 250;
+    private int x = 100;
+    private int y = 500;
     private int deltaX = 5;
     private Image image;
+    private Image cesto;
     private int panelWidth;
     private int panelHeight;
+
+    int newWidth = 80;
+    int newHeight = 80;
+
 
     public CriancaAnimacao() {
         // Carrega a imagem
         image = new ImageIcon("src/main/java/gui/resources/assets//criancaComBola/crianca1LadoD1.png").getImage();
 
+        cesto = new ImageIcon("src/main/java/gui/resources/assets/cesto/cesto5.png").getImage();
         // Configura o temporizador para atualizar a posição da imagem
-        timer = new Timer(50, this);
+        timer = new Timer(25, this);
         timer.start();
 
 
@@ -36,8 +42,11 @@ public class CriancaAnimacao extends JPanel implements ActionListener {
         Image bp = new ImageIcon("src/main/java/gui/resources/assets/quadra.jpg").getImage();
         g2d.drawImage(bp, 0, 0, getWidth(), getHeight(), null);
 
+
+        g2d.drawImage(cesto, getWidth() / 2 -50, 500, newWidth, newHeight, null);
+
         // Desenha a imagem na posição atual
-        g2d.drawImage(image, x, y, null);
+        g2d.drawImage(image, x, y,newWidth,newHeight, null);
     }
 
     @Override
@@ -55,12 +64,12 @@ public class CriancaAnimacao extends JPanel implements ActionListener {
         }
 
         // Verifica se a imagem ultrapassou o limite esquerdo do painel
-        if (x < 0) {
+        if (x < 100) {
             // Inverte a direção do movimento da imagem
             deltaX = -deltaX;
             image = new ImageIcon("src/main/java/gui/resources/assets//criancaComBola/crianca1LadoD1.png").getImage();
             // Reposiciona a imagem para a borda esquerda do painel
-            x = 0;
+            x = 100;
         }
 
         // Redesenha o painel com a nova posição da imagem
@@ -70,7 +79,7 @@ public class CriancaAnimacao extends JPanel implements ActionListener {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Animated Image");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 350);
+        frame.setSize(1200, 700);
 
         // Adiciona o painel ao frame
         frame.add(new CriancaAnimacao());
